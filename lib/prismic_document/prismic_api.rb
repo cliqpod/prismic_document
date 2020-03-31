9fed91615c904068b4d95320ff5415eed615ca09
Slavik <pevmel@gmail.com>
2020-03-31 18:51:57 +0300

    new request methods

--- lib/prismic_document/prismic_api.rb
+++ lib/prismic_document/prismic_api.rb
@@ -25,12 +25,35 @@ class PrismicDocument::PrismicApi
     end
   end
 
+  def by_document_type_and_locale(request_domain, doc_type, locale = 'en', options: {})
+    PrismicDocument::Retry.call(default: []) do
+      query([
+                Prismic::Predicates.at('document.type', doc_type.to_s),
+                Prismic::Predicates.at("my.#{doc_type}.domain", request_domain),
+                { "lang" => locale }
+            ],
+            { 'orderings' => '[document.first_publication_date desc]' }.merge(options)).results.map { |x| PrismicDocument::Page.new(object: x, type: doc_type)}
+    end
+  end
+
   def by_path(request_domain, doc_type, path)
     PrismicDocument::Retry.call(default: nil) do
       doc = query([Prismic::Predicates.at("my.#{doc_type}.domain", request_domain), Prismic::Predicates.at("my.#{doc_type}.path", path.to_s)]).results&.first
       PrismicDocument::Page.new(object: doc, type: doc_type)
     end
   end
+
+  def by_path_and_locale(request_domain, doc_type, locale = 'en', path)
+    PrismicDocument::Retry.call(default: nil) do
+      doc = query([
+                      Prismic::Predicates.at("my.#{doc_type}.domain", request_domain),
+                      Prismic::Predicates.at("my.#{doc_type}.path", path.to_s),
+                      { "lang" => locale }
+                  ]).results&.first
+      PrismicDocument::Page.new(object: doc, type: doc_type)
+    end
+  end
+
 
   def get_values_by_type(domain, type, field_name)
     results = query(Prismic::Predicates.any("my.#{type}.domain", [domain])).results
