1faba8ad121a135aeff4258e73ba2b7244e95cff
Slavik <pevmel@gmail.com>
2020-04-01 13:36:48 +0300

    added by_pat_and_locale method

--- lib/prismic_document/prismic_api.rb
+++ lib/prismic_document/prismic_api.rb
@@ -4,7 +4,7 @@ class PrismicDocument::PrismicApi
   include Singleton
 
   class << self
-    delegate :by_document_type, :by_path, :get_values_by_type, :reload_client, to: :instance
+    delegate :by_document_type, :by_path, :by_path_and_locale, :get_values_by_type, :reload_client, to: :instance
   end
 
 
@@ -25,17 +25,6 @@ class PrismicDocument::PrismicApi
     end
   end
 
-  def by_document_type_and_locale(request_domain, doc_type, locale = 'en', options: {})
-    PrismicDocument::Retry.call(default: []) do
-      query([
-                Prismic::Predicates.at('document.type', doc_type.to_s),
-                Prismic::Predicates.at("my.#{doc_type}.domain", request_domain),
-                { "lang" => locale }
-            ],
-            { 'orderings' => '[document.first_publication_date desc]' }.merge(options)).results.map { |x| PrismicDocument::Page.new(object: x, type: doc_type)}
-    end
-  end
-
   def by_path(request_domain, doc_type, path)
     PrismicDocument::Retry.call(default: nil) do
       doc = query([Prismic::Predicates.at("my.#{doc_type}.domain", request_domain), Prismic::Predicates.at("my.#{doc_type}.path", path.to_s)]).results&.first
@@ -43,17 +32,15 @@ class PrismicDocument::PrismicApi
     end
   end
 
-  def by_path_and_locale(request_domain, doc_type, locale = 'en', path)
+  def by_path_and_locale(request_domain, doc_type, path, locale = 'en')
     PrismicDocument::Retry.call(default: nil) do
       doc = query([
                       Prismic::Predicates.at("my.#{doc_type}.domain", request_domain),
-                      Prismic::Predicates.at("my.#{doc_type}.path", path.to_s),
-                      { "lang" => locale }
-                  ]).results&.first
+                      Prismic::Predicates.at("my.#{doc_type}.path", path.to_s)
+                  ], { "lang" => locale }).results&.first
       PrismicDocument::Page.new(object: doc, type: doc_type)
     end
   end
-
 
   def get_values_by_type(domain, type, field_name)
     results = query(Prismic::Predicates.any("my.#{type}.domain", [domain])).results
