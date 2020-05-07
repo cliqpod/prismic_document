861faf389077d2181c5ae60fb1c8998186402c95
Slavik <pevmel@gmail.com>
2020-05-07 14:32:30 +0300

    Added link_resolver to Serializers

--- lib/prismic_document/html_serializer.rb
+++ lib/prismic_document/html_serializer.rb
@@ -1,39 +1,35 @@
-PrismicDocument::TextHTMLSerializer = lambda do |domain|
+PrismicDocument::TextHTMLSerializer = lambda do |domain, link_resolver = nil|
   Prismic.html_serializer do |element, html|
-    link_resolver ||= nil
     case element
     when Prismic::Fragments::StructuredText::Block::Preformatted
       element.text
     when Prismic::Fragments::StructuredText::Span::Hyperlink
       link = element.link
-      if link.is_a?(Prismic::Fragments::DocumentLink) && link.broken
-        "<span>#{html}</span>"
+      if link.is_a?(Prismic::Fragments::DocumentLink)
+        return "<span>#{html}</span>" if link.broken
+        %(<a href="#{link.url(link_resolver)}">#{html}</a>)
       else
         target_value = link.target || '_blank' unless link.url.include? domain
         target = target_value.nil? ? "" : 'target="' + target_value + '" rel="nofollow"'
-        %(<a href="#{link.url(link_resolver)}" #{target}>#{html}</a>)
+        %(<a href="#{link.url}" #{target}>#{html}</a>)
       end
-    else
-      nil
     end
   end
 end
 
-PrismicDocument::HTMLSerializer = lambda do |domain|
+PrismicDocument::HTMLSerializer = lambda do |domain, link_resolver = nil|
   Prismic.html_serializer do |element, html|
-    link_resolver ||= nil
     case element
     when Prismic::Fragments::StructuredText::Span::Hyperlink
       link = element.link
-      if link.is_a?(Prismic::Fragments::DocumentLink) && link.broken
-        "<span>#{html}</span>"
+      if link.is_a?(Prismic::Fragments::DocumentLink)
+        return "<span>#{html}</span>" if link.broken
+        %(<a href="#{link.url(link_resolver)}">#{html}</a>)
       else
         target_value = link.target || '_blank' unless link.url.include? domain
         target = target_value.nil? ? "" : 'target="' + target_value + '" rel="nofollow"'
-        %(<a href="#{link.url(link_resolver)}" #{target}>#{html}</a>)
+        %(<a href="#{link.url}" #{target}>#{html}</a>)
       end
-    else
-      nil
     end
   end
 end
