31e2f20324463d4ae7c4553aa3ff3c1c826b9c1b
sarco3t <14gainward88@gmail.com>
2021-07-20 13:51:40 +0300

    prismic interlinking fix

--- lib/prismic_document/interlink_service.rb
+++ lib/prismic_document/interlink_service.rb
@@ -29,6 +29,7 @@ class PrismicDocument::InterlinkService
         else
           doc = Nokogiri::HTML::fragment(text)
           doc.search('.//text()[not(ancestor::h1 or ancestor::h2 or ancestor::h3 or ancestor::h4 or ancestor::h5 or ancestor::h6)]').select { |x| x.content.present? }.each do |node|
+            next if node.parent&.name == 'a'
             node.replace(node.content.gsub(/(?!<(a|h1|h2|h3|h4|h5|h6|a)[^>]*>)(?<!\w)(?<foo>#{Regexp.escape(keyword)})(?!\w)(?![^<]*<\/(a|h1|h2|h3|h4|h5|h6)>)/i, "<a href=\"#{path.chomp('/')}/\">\\k<foo></a>"))
           end
 
